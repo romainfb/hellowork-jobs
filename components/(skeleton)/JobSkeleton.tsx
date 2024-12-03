f57cf1396c92dfb0e6a7909805e7888f87683dd4
Tryphoz PC <romainfb24pro@gmail.com>
2024-12-03 20:42:11 +0100

    feat(Dynamization) Implements data with API

---
+++ components/(skeleton)/JobSkeleton.tsx
@@ -0,0 +1,7 @@
+const JobSkeleton = () => {
+  return (
+    <div className="flex flex-col text-clip rounded-xl w-96 h-[25rem] bg-gray-100 animate-pulse"></div>
+  );
+};
+
+export default JobSkeleton;
